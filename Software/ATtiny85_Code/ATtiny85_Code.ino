1eb929e5493d139e815545fbaf24787553090f7f
Liam Smolenaars <liam.great98@gmail.com>
2020-12-28 22:18:09 -0500

    Update ATtiny85_Code.ino

--- Software/ATtiny85_Code/ATtiny85_Code.ino
+++ Software/ATtiny85_Code/ATtiny85_Code.ino
@@ -1,6 +1,15 @@
-void setup() {
-  // put your setup code here, to run once:
+// Pin 0 - Data
+// Pin 1 - Clock
+// Pin 2 - SR Signal
+// Pin 3 - Gate
+// Pin 4 - Button
 
+void setup() {
+  pinMode(0, INPUT);
+  pinMode(1, INPUT);
+  pinMode(2, INPUT);
+  pinMode(3, OUTPUT);
+  pinMode(4, INPUT);
 }
 
 void loop() {
